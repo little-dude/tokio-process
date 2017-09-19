5ba7832f3629384bf9b1d1a9ab33c429ce2d4b16
Travis CI User <travis@example.org>
2017-09-19 00:34:05 +0000

    Documentation for alexcrichton/tokio-process@dff247f

---
+++ implementors/tokio_io/trait.AsyncRead.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors["tokio_process"] = ["impl <a class=\"trait\" href=\"https://docs.rs/tokio-io/0.1/tokio_io/trait.AsyncRead.html\" title=\"trait tokio_io::AsyncRead\">AsyncRead</a> for <a class=\"struct\" href=\"tokio_process/struct.ChildStdout.html\" title=\"struct tokio_process::ChildStdout\">ChildStdout</a>","impl <a class=\"trait\" href=\"https://docs.rs/tokio-io/0.1/tokio_io/trait.AsyncRead.html\" title=\"trait tokio_io::AsyncRead\">AsyncRead</a> for <a class=\"struct\" href=\"tokio_process/struct.ChildStderr.html\" title=\"struct tokio_process::ChildStderr\">ChildStderr</a>",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
---
+++ implementors/tokio_io/trait.AsyncWrite.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors["tokio_process"] = ["impl <a class=\"trait\" href=\"https://docs.rs/tokio-io/0.1/tokio_io/trait.AsyncWrite.html\" title=\"trait tokio_io::AsyncWrite\">AsyncWrite</a> for <a class=\"struct\" href=\"tokio_process/struct.ChildStdin.html\" title=\"struct tokio_process::ChildStdin\">ChildStdin</a>",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
